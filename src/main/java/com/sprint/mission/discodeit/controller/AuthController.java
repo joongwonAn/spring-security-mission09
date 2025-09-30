58ff0f3f1d9d9002323ce2b9227edaa669594a54
joongwon <anjoongwon517@gmail.com>
2025-09-30 14:21:35 +0900

    feat: CSRF 토큰 발급 API 구현
    - API 스펙
        - 엔드포인트: GET /api/auth/csrf-token
        - 요청: 없음
        - 응답: 203 Void

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -8,10 +8,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -20,15 +18,25 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/auth")
 public class AuthController implements AuthApi {
 
-  private final AuthService authService;
+    private final AuthService authService;
 
-  @PostMapping(path = "login")
-  public ResponseEntity<UserDto> login(@RequestBody @Valid LoginRequest loginRequest) {
-    log.info("로그인 요청: username={}", loginRequest.username());
-    UserDto user = authService.login(loginRequest);
-    log.debug("로그인 응답: {}", user);
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(user);
-  }
+    @PostMapping(path = "login")
+    public ResponseEntity<UserDto> login(@RequestBody @Valid LoginRequest loginRequest) {
+        log.info("로그인 요청: username={}", loginRequest.username());
+        UserDto user = authService.login(loginRequest);
+        log.debug("로그인 응답: {}", user);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(user);
+    }
+
+    @GetMapping("/csrf-token")
+    public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) { // CsrfToken 파라미터를 메서드 인자로 선언하면, HandlerMethodArgumentResolver를 통해 자동으로 주입됨
+        String tokenValue = csrfToken.getToken(); // GET 요청에는 CSRF 인증이 이루어지지 않기 때문에 토큰 초기화 X, 따라서 명시적으로 메소드에서 토큰 호출
+        log.debug("CSRF 토큰 요청: {}", tokenValue);
+
+        return ResponseEntity
+                .status(HttpStatus.NON_AUTHORITATIVE_INFORMATION)
+                .build();
+    }
 }
