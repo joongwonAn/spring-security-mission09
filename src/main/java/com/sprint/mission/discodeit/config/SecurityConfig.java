ebf49e5988d5e697c87b2b61e4e1e239bf5614cf
joongwon <anjoongwon517@gmail.com>
2025-09-30 16:15:34 +0900

    feat: formLogin을 기본값으로 활성화
    - 필터 목록
    Security filter chain: [
      DisableEncodeUrlFilter
      WebAsyncManagerIntegrationFilter
      SecurityContextHolderFilter
      HeaderWriterFilter
      CsrfFilter
      LogoutFilter
      UsernamePasswordAuthenticationFilter // 추가
      DefaultResourcesFilter // 추가
      DefaultLoginPageGeneratingFilter // 추가
      DefaultLogoutPageGeneratingFilter // 추가
      RequestCacheAwareFilter
      SecurityContextHolderAwareRequestFilter
      AnonymousAuthenticationFilter
      ExceptionTranslationFilter
    ]

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.config;
 import com.sprint.mission.discodeit.config.handler.SpaCsrfTokenRequestHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,6 +22,7 @@ public class SecurityConfig {
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // csrf 토큰 저장소를 쿠키로 지정
                         .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()) // csrf 토큰을 읽고 검증하기 위해 사용
                 )
+                .formLogin(Customizer.withDefaults())
         ;
         return http.build();
     }
