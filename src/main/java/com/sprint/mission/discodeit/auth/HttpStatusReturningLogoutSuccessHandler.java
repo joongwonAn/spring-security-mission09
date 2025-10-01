295d860ea5368abf7c121337d12379eda39ddee0
joongwon <anjoongwon517@gmail.com>
2025-10-01 16:08:23 +0900

    feat: LogoutSuccessHandler 컴포넌트를 대체 할 HttpStatusReturningLogoutSuccessHandler 구현

---
+++ src/main/java/com/sprint/mission/discodeit/auth/HttpStatusReturningLogoutSuccessHandler.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.discodeit.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class HttpStatusReturningLogoutSuccessHandler implements LogoutSuccessHandler {
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        log.debug("Logout successful for user: {}", authentication.getName());
+        response.setStatus(HttpServletResponse.SC_NO_CONTENT); // 204 Void 반환
+    }
+}
