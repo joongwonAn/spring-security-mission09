50499fa29c696df91fbc20824a5a3bd8ec393664
joongwon <anjoongwon517@gmail.com>
2025-10-01 16:31:02 +0900

    feat: 권한 정의(ADMIN /  CHANNEL_MANAGER / USER)

--- src/main/java/com/sprint/mission/discodeit/entity/User.java
+++ src/main/java/com/sprint/mission/discodeit/entity/User.java
@@ -20,40 +20,49 @@ import lombok.Setter;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)  // JPA를 위한 기본 생성자
 public class User extends BaseUpdatableEntity {
 
-  @Column(length = 50, nullable = false, unique = true)
-  private String username;
-  @Column(length = 100, nullable = false, unique = true)
-  private String email;
-  @Column(length = 60, nullable = false)
-  private String password;
-  @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-  @JoinColumn(name = "profile_id", columnDefinition = "uuid")
-  private BinaryContent profile;
-  @JsonManagedReference
-  @Setter(AccessLevel.PROTECTED)
-  @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-  private UserStatus status;
+    @Column(length = 50, nullable = false, unique = true)
+    private String username;
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+    @Column(length = 60, nullable = false)
+    private String password;
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", columnDefinition = "uuid")
+    private BinaryContent profile;
+    @JsonManagedReference
+    @Setter(AccessLevel.PROTECTED)
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private UserStatus status;
+    @Column(length = 20, nullable = false)
+    private Role role;
 
-  public User(String username, String email, String password, BinaryContent profile) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-    this.profile = profile;
-  }
+    public User(String username, String email, String password, BinaryContent profile, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.profile = profile;
+        this.role = role;
+    }
 
-  public void update(String newUsername, String newEmail, String newPassword,
-      BinaryContent newProfile) {
-    if (newUsername != null && !newUsername.equals(this.username)) {
-      this.username = newUsername;
+    public void update(String newUsername, String newEmail, String newPassword,
+                       BinaryContent newProfile) {
+        if (newUsername != null && !newUsername.equals(this.username)) {
+            this.username = newUsername;
+        }
+        if (newEmail != null && !newEmail.equals(this.email)) {
+            this.email = newEmail;
+        }
+        if (newPassword != null && !newPassword.equals(this.password)) {
+            this.password = newPassword;
+        }
+        if (newProfile != null) {
+            this.profile = newProfile;
+        }
     }
-    if (newEmail != null && !newEmail.equals(this.email)) {
-      this.email = newEmail;
+
+    public void updateRole(Role newRole) {
+        if (newRole != null) {
+            this.role = newRole;
+        }
     }
-    if (newPassword != null && !newPassword.equals(this.password)) {
-      this.password = newPassword;
-    }
-    if (newProfile != null) {
-      this.profile = newProfile;
-    }
-  }
 }
