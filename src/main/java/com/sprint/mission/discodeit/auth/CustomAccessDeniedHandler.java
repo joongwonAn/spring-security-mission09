d8e790c31b1d9072e908e29ccb8379c4b371c819
joongwon <anjoongwon517@gmail.com>
2025-10-02 17:44:59 +0900

    feat: 적절한 권한이 없으면 403 반환

---
+++ src/main/java/com/sprint/mission/discodeit/auth/CustomAccessDeniedHandler.java
@@ -0,0 +1,29 @@
+package com.sprint.mission.discodeit.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.error("AccessDeniedException", accessDeniedException);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write("""
+                    {
+                      "code": "FORBIDDEN",
+                      "message": "접근 권한 없음"
+                    }
+                """);
+    }
+}
